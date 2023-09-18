fca494c1b5b5503c46b21adf30772cda069a5a90
OguzhnPr <oguzhanparlakbey@gmail.com>
2023-09-19 00:26:08 +0200

    v2

--- src/main/java/com/hpw/contactmessage/ContactMessage.java
+++ src/main/java/com/hpw/contactmessage/ContactMessage.java
@@ -1,18 +1,30 @@
 package com.hpw.contactmessage;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
+
 @Entity
 public class ContactMessage{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
+    private String name;
+    @NotNull
+    private String email;
+    @NotNull
+    private String subject;
+    @NotNull
     private String message;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+
 
 }
--- src/main/java/com/hpw/contactmessage/ContactMessageController.java
+++ src/main/java/com/hpw/contactmessage/ContactMessageController.java
@@ -1,9 +1,20 @@
 package com.hpw.contactmessage;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/contactMessages")
+
 public class ContactMessageController {
+
+    //1- save()
+    //https//localhost:8080/contactMessages/save
+    //@PostMapping("save")
+
+
+
 
 }
