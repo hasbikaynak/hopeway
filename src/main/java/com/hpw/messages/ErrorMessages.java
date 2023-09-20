d09f854744695120c0678f1f2a6b68861788ce8a
OguzhnPr <oguzhanparlakbey@gmail.com>
2023-09-20 14:48:33 +0200

    istenmeyen bosluklar ve yorum satirlari temizlendi

--- src/main/java/com/hpw/messages/ErrorMessages.java
+++ src/main/java/com/hpw/messages/ErrorMessages.java
@@ -4,7 +4,5 @@ public class ErrorMessages {
 
     private ErrorMessages() {
     }
-    //for Service, save()
     public static final String ALREADY_SEND_A_MESSAGE_TODAY="Error : You have already send a message with this e-mail ";
-
 }
--- src/main/java/com/hpw/messages/SuccessMessages.java
+++ src/main/java/com/hpw/messages/SuccessMessages.java
@@ -1,12 +1,6 @@
 package com.hpw.messages;
 
 public class SuccessMessages {
-    private SuccessMessages() {
-    }
-
-
+    private SuccessMessages() {}
     public static final String CONTACT_MESSAGE_SAVED_SUCCESSFULLY= "Contact Message Created Successfully ";
-
-
-
 }
