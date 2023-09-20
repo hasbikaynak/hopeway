c019616a278d6d09555ccb394bb84390983001b0
OguzhnPr <oguzhanparlakbey@gmail.com>
2023-09-20 14:34:07 +0200

    degisiklik

--- src/main/java/com/hpw/messages/ErrorMessages.java
+++ src/main/java/com/hpw/messages/ErrorMessages.java
@@ -4,5 +4,7 @@ public class ErrorMessages {
 
     private ErrorMessages() {
     }
+    //for Service, save()
+    public static final String ALREADY_SEND_A_MESSAGE_TODAY="Error : You have already send a message with this e-mail ";
 
 }
--- src/main/java/com/hpw/messages/SuccessMessages.java
+++ src/main/java/com/hpw/messages/SuccessMessages.java
@@ -5,4 +5,8 @@ public class SuccessMessages {
     }
 
 
+    public static final String CONTACT_MESSAGE_SAVED_SUCCESSFULLY= "Contact Message Created Successfully ";
+
+
+
 }
