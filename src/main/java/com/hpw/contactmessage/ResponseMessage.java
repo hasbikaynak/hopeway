4cb3d6899b17ece646089026d66ec4429560b82b
OguzhnPr <oguzhanparlakbey@gmail.com>
2023-09-20 14:59:06 +0200

    ResponseMessage.java space has been deleted

--- src/main/java/com/hpw/contactmessage/ResponseMessage.java
+++ src/main/java/com/hpw/contactmessage/ResponseMessage.java
@@ -4,12 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import org.springframework.http.HttpStatus;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)  // null olmaqyan fieldlar JSON dosyaya dahil edilecek-> sadece null olmayanlar
+@JsonInclude(JsonInclude.Include.NON_NULL)
+// null olmaqyan fieldlar JSON dosyaya dahil edilecek-> sadece null olmayanlar
 @Builder(toBuilder = true)
-public record ResponseMessage<E> (E object,
-                                  String message,
-                                  HttpStatus httpStatus) {
+public record ResponseMessage<E>(E object, String message, HttpStatus httpStatus) {
 }
-
-
-//Burasini her nerede kullanirsam kullanayim ona uyum saglayacak, baska tarafta da kullanabilirim
