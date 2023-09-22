e4a1975e2d3a5f32d8dc0beaa9fe7ef110b78b1f
NurKubra <kubraanurcevik@outlook.de>
2023-09-22 16:43:44 +0200

    helper and mapper created

--- src/main/java/com/hpw/exception/IllegalArgumentException.java
+++ src/main/java/com/hpw/exception/IllegalArgumentException.java
@@ -8,4 +8,7 @@ public class IllegalArgumentException extends RuntimeException{
     public IllegalArgumentException(String message) {
         super(message);
     }
+
+
+
 }
--- src/main/java/com/hpw/messages/ErrorMessages.java
+++ src/main/java/com/hpw/messages/ErrorMessages.java
@@ -5,4 +5,10 @@ public class ErrorMessages {
     private ErrorMessages() {
     }
     public static final String ALREADY_SEND_A_MESSAGE_TODAY="Error : You have already send a message with this e-mail ";
+    public static final String INVALID_PAGE_OR_SIZE_VALUE="Invalid page or size value. Page and size values must be positive!";
+    public static final String INVALID_TYPE_VALUE ="Invalid type value. Type value 'asc' or 'desc'!";
+    public static final String INVALID_SORT_FIELD="Invalid sort field. The sort field cannot be empty!";
+
+
+
 }
