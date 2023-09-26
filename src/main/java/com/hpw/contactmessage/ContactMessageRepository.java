59d7ab72e64250c6b7e827209e2d681a1e6fb7de
kaynak <kaynakhasbi1@gmail.com>
2023-09-26 14:07:24 +0200

    unused imports has been deleted

--- src/main/java/com/hpw/contactmessage/ContactMessageRepository.java
+++ src/main/java/com/hpw/contactmessage/ContactMessageRepository.java
@@ -1,13 +1,11 @@
 package com.hpw.contactmessage;
 
-import io.micrometer.core.instrument.binder.db.MetricsDSLContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Repository
 public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
